7ab7e2acb2183e7bbcbebb5afd4ad9003665ec1f
DariusPlaczek <dariusplaczek@gmail.com>
2020-11-21 19:51:15 +0100

    Practice with MaterialUI - Added Answer function

--- src/components/MaterialUI.js
+++ src/components/MaterialUI.js
@@ -1,76 +1,90 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Input from '@material-ui/core/Input';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-
 function MaterialUI() {
-    const [open, setOpen] = useState(false);
-    const [title, setTitle] = useState('Tytuł');
-    const [content, setContent] = useState('Zmien swoj Tytuł i Treści ');
-    const [cacheTitle, setCacheTitle] = useState('');
-    const [cacheContent, setCacheContent] = useState('');
+  const [open, setOpen] = useState(false);
+  const [answerStyle, setAnswerStyle] = useState('none');
+	const [answerBG, setAnswerBG] = useState('red')
+  const [answerText, setAnswerText] = useState('Zla odpowiedz')
 
-    const handleClickOpen = () => {
-      setOpen(true);
-    };
+  const styles = {
+      display: answerStyle,
+			backgroundColor: answerBG,
+			color: '#FFFFFF',
+			fontSize: 40,
+			marginTop: 25
+  }
 
-    const handleClose = () => {
-      setOpen(false);
-      setCacheTitle('');
-      setCacheContent('');
-    };
+  useEffect(() => {
+    if (answerStyle !== 'none') {
+        setTimeout(() => {
+            setAnswerStyle('none')
+        }, 2500);
+    }
+  }, [answerStyle])
 
-    const handleChange = () => {
-      setOpen(false);
-      setTitle(cacheTitle);
-      setContent(cacheContent);
-    };
 
-    return (
-        <>
-        <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Dialog
-       </Button>
-        <Dialog open={open} onClose={handleChange} aria-labelledby="form-dialog-title">
-         <DialogTitle id="form-dialog-title">{title}</DialogTitle>
-            <DialogContent>
-            <DialogContentText>
-                {content}
-            </DialogContentText>
-            <Input
-                autoFocus
-                id="title"
-                margin="dense"
-                label="Tytuł"
-                type="title"
-                onChange={(event) => setCacheTitle(event.target.value)}
-            />
-            <TextField
-                margin="dense"
-                id="conten"
-                label="Treści"
-                type="content"
-                onChange={(event) => setCacheContent(event.target.value)}
-                fullWidth
-            />
-            </DialogContent>
-            <DialogActions>
-            <Button onClick={handleChange} color="primary">
-                SUBMIT
-            </Button>
-            <Button onClick={handleClose} color="primary">
-                X
-            </Button>
-            </DialogActions>
-        </Dialog>
-        </>
-    )
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const falseAnswer = () => {
+    setOpen(false);
+    setAnswerStyle('block');
+		setAnswerBG('red');
+		setAnswerText('Zla odpowiedz');
+
+  };
+
+  const rightAnswer = () => {
+  setOpen(false);
+	setAnswerStyle('block');
+	setAnswerBG('green');
+  setAnswerText('Dobra Odpowiedz');
+};
+
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+      Otwórz mnie
+      </Button>
+      <Dialog
+        open={open}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">HODOR IPSUM</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Hodor. Hodor hodor, hodor. Hodor hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor; hodor hodor hodor. Hodor. Hodor hodor; hodor hodor - hodor, hodor, hodor hodor. Hodor, hodor. Hodor. Hodor, hodor hodor hodor; hodor hodor; hodor hodor hodor! Hodor hodor HODOR! Hodor hodor... Hodor hodor hodor...
+          </DialogContentText>
+          <DialogContentText id="alert-dialog-description">
+            Jaka jest prawidłowa odpowiedz?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={falseAnswer} color="primary">
+          HARRY POTTER
+          </Button>
+          <Button onClick={rightAnswer} color="primary" >
+            GRA O TRON
+          </Button>
+          <Button onClick={falseAnswer} color="primary">
+            PULP FICTION
+          </Button>
+          <Button onClick={falseAnswer} color="primary">
+            FORREST GUMPT
+          </Button>
+        </DialogActions>
+      </Dialog>
+        <div className="MuiButton-root" style={styles}>{answerText}</div>
+    </div>
+  );
 }
 
 export default MaterialUI
