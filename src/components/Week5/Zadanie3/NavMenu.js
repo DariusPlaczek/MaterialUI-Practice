c51ae815af1a6c585293ee6b25361e733eca8889
DariusPlaczek <dariusplaczek@gmail.com>
2020-11-24 11:44:37 +0100

    change the primary and secondery color

--- src/components/Week5/Zadanie3/NavMenu.js
+++ src/components/Week5/Zadanie3/NavMenu.js
@@ -13,15 +13,3 @@ function NavMenu() {
 }
 
 export default NavMenu
-
-
-
-// 3. Menu nawigacyjne
-// Napisz komponent NavMenu, który będzie pojawiał się po lewej stronie aplikacji. Komponent będzie zawierał linki nawigacyjne, domyślnie będzie ukryty.
-
-// W aplikacji osadź przycisk, który będzie pozwalał na otworzenie menu (pierwszy klik) oraz zamknięcie menu (kliknięcie po raz drugi).
-
-// Podobny przycisk osadź w komponencie NavMenu. Po jego kliknięciu menu będzie się ukrywało.
-
-// Modyfikacja
-// Za pomocą React Router DOM dodaj kilka podstron: np. /home, /about, /contact. W komponencie Nav
