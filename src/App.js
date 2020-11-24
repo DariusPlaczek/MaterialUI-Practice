63f0793fa501d2c0b3c77199945f28a361e3ff8c
DariusPlaczek <dariusplaczek@gmail.com>
2020-11-24 20:33:15 +0100

    cleaned the code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import './App.css';
 
-import {Home, Week5, Contact } from './components/Pages';
+import { Home, Week5, Contact } from './components/Pages';
 import { LeftSideBar, Content, ToogleBar } from './components/Container'
 
 function App() {
@@ -20,7 +20,6 @@ function App() {
             <Route exact path='/' component={Home} />
             <Route path='/week5' component={Week5} />
             <Route path='/contact' component={Contact} />
-            <Route path='/users' component={Contact} />
           </Switch>
         </Content>
         <ToogleBar />
--- src/components/Pages/index.js
+++ src/components/Pages/index.js
@@ -1,4 +1,3 @@
 export { default as Home } from './Home';
 export { default as Contact } from './Contact';
 export { default as Week5 } from './Week5';
-export { default as Users } from './Users';
